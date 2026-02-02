d2d5826b98abf6f163ab863bb3f0a8626d4d4c30
UPMSCD <connor.e.davis.02@gmail.com>
2026-02-01 21:02:43 -0800

    Add files via upload

--- CxCC_ZFold_oaps.py
+++ CxCC_ZFold_oaps.py
@@ -25,59 +25,33 @@ def calculate_oap_zemax_params(d_in, magnification, f1_parent):
 
 
 
-    #################################################
-    # The Anamorphic Width (The "Stretch" on the mirror surface)
-    # For 90 degree fold, the beam hits at 45 degrees.
-    y_footprint_half_m2 = (d_in * magnification / 2) * math.sqrt(2)
-
-    h = d_in / 2 #semi diameter
-    y_top_ray = r2 * (h / (r1 - h))
-    y_bot_ray = r2 * (-h / (r1 + h))
-    
-    real_y_width_m2 = y_top_ray - y_bot_ray
-    real_y_shift_m2 = (y_top_ray + y_bot_ray) / 2
-    # 3. Anamorphic Stretch Factor
-    y_stretch_factor = real_y_width_m2 / (d_in * magnification)
-    Anamorphic_stretch_Ratio = f"{round(((y_stretch_factor - 1) * 100) , 4)}%"
-
-    x_compression_factor = 1 / y_stretch_factor
-    x_min_half_width_m2 = (d_in * magnification / 2) * x_compression_factor
-
-    System_Anamorphic_Ratio = y_stretch_factor / x_compression_factor
-    #################################################
-
-
-
 
 
     #################################################
     #M1
     x_half_width = (d_in / 2)
     y_half_width = x_half_width
-    m1_LDE_Decenter_Y = abs(r1) * (1 + ((magnification - 1) * 0.5))
+    m1_LDE_Decenter_Y = abs(r1) * (1 + ((magnification - 1) * 0.5)) - abs(r1)
     #################################################
-
 
 
 
     #################################################
     # For the second mirror, the beam is stretched and shifted
-    x_half_width_m2 = (d_in * magnification / 2) * y_stretch_factor
+    x_half_width_m2 = (d_in * magnification / 2)
     y_half_width_m2 = x_half_width_m2
-    m2_LDE_Decenter_Y = (abs(r2) * -1) - real_y_shift_m2
-    Aperture_Decenter_Y_m2 = (abs(r2) * -1) - real_y_shift_m2
     #################################################
 
     #################################################
     #DT stuff
-    DT_Swing_Radius = abs(r1) + separation_dist + y_half_width_m2 + real_y_shift_m2
+    DT_Swing_Radius = abs(r1) + separation_dist + y_half_width_m2
     DT_Swing_Radius_edge = math.sqrt(DT_Swing_Radius**2 + x_half_width_m2**2)
 
     DT_Swing_Radius_m1_inside = abs(r1) - x_half_width
     DT_Swing_Radius_m1_outside = abs(r1) + x_half_width
     DT_Swing_Radius_m1_edge = math.sqrt(DT_Swing_Radius_m1_outside**2 + x_half_width**2)
 
-    DT_Swing_Radius_m2_inside = abs(r1) + separation_dist - y_half_width_m2 + real_y_shift_m2
+    DT_Swing_Radius_m2_inside = abs(r1) + separation_dist - y_half_width_m2
     #################################################
 
 
@@ -97,13 +71,10 @@ def calculate_oap_zemax_params(d_in, magnification, f1_parent):
         "M2": {
             "ROC": r2, # Positive for concave
             "Conic": -1,
-            "Aperture_Decenter_Y": Aperture_Decenter_Y_m2,
+            "Aperture_Decenter_Y": r2,
             "X_Half_Width": x_half_width_m2,
             "Y_Half_Width": y_half_width_m2,
-            "image_LDE_Decenter_y": m2_LDE_Decenter_Y,
-            "X_Min_Half_Width":x_min_half_width_m2,
-            "Anamorphic_stretch_Ratio_Y":Anamorphic_stretch_Ratio,
-            "System_Anamorphic_Ratio":System_Anamorphic_Ratio,
+            "image_LDE_Decenter_y": r2,
 
         },
         "Diamond_Turning_Swing_Radius_at_center":DT_Swing_Radius,
@@ -119,7 +90,7 @@ def calculate_oap_zemax_params(d_in, magnification, f1_parent):
 
 # --- Example Run (Using your EO numbers) ---
 # EO has R1=50.8, so f1=25.4. Magnification = 2.
-params = calculate_oap_zemax_params(d_in=55, magnification=2.25, f1_parent=46)
+params = calculate_oap_zemax_params(d_in=50, magnification=2.25, f1_parent=51.5)
 
 
 
